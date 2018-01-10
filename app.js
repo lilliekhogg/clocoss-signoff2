8ebf0b78d9e2d905bda6f888ee1f76e6f475da46
lilliekhogg <lilliekhogg@gmail.com>
2018-01-10 15:46:07 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use('/api', require('./api'));
+
+app.use(express.static('static', { extensions: ['html'] }));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, (err) => {
+  if (err) console.log('error', err);
+  else console.log(`app listening on port ${port}`);
+});
