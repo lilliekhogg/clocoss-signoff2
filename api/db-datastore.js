06e2861c89bddd271662850ba8fcd9aa7a72f7bf
lilliekhogg <lilliekhogg@gmail.com>
2018-01-11 12:20:59 +0000

    comments

--- api/db-datastore.js
+++ api/db-datastore.js
@@ -1,17 +1,30 @@
+//requiring cloud datastore with given name
 const datastore = require('@google-cloud/datastore')({ namespace: 'lillie-clocoss2' });
 
-const kind = 'files';
+const kind = 'fileCount';
 
+//returning the datastore key
 function key(id) {
   return datastore.key([kind, id]);
 }
 
+//returning name
 module.exports.get = async (id) => {
   const [data] = await datastore.get(key(id));
   if (data && data.val) return `${data.val}`;
   return '0';
 };
 
+//overwriting the current value and updating counter.
+module.exports.put = async (id, val) => {
+  const entity = {
+    key: key(id),
+    data: { name: id, val },
+  }
+  await datastore.save(entity);
+};
+
+//adding values on top of the existing name, function replaces value with total when there is existing value.
 module.exports.post = async (id, val) => {
     const [data] = await datastore.get(key(id));
     if (data && data.val) {
@@ -22,18 +35,11 @@ module.exports.post = async (id, val) => {
         data: { name: id, val },
       }
       await datastore.save(entity);
+  //returning the total value
       return `${val}`;
 };
 
-
-module.exports.put = async (id, val) => {
-  const entity = {
-    key: key(id),
-    data: { name: id, val },
-  }
-  await datastore.save(entity);
-};
-
+//completely removes the key
 module.exports.delete = async(id) => {
   const [data] = await datastore.delete(key(id));
   // check if updated.
