1bb1c38cad7f1cb3a6f95d01a8a7c5cacd706e00
lilliekhogg <lilliekhogg@gmail.com>
2018-01-11 12:28:32 +0000

    add comments

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 'use strict';
-
+//require express
 const express = require('express');
 
 const app = express();
 
+//set location for api folder
 app.use('/api', require('./api'));
 
+//define location for static folder
 app.use(express.static('static', { extensions: ['html'] }));
 
 const port = process.env.PORT || 8080;
 
+//run the app
 app.listen(port, (err) => {
   if (err) console.log('error', err);
   else console.log(`app listening on port ${port}`);
